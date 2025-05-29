14b6e9981c52245bede6ce71b6e6803db9aa2578
danielfranchi555 <danielfranchi3@gmail.com>
2025-05-29 17:09:08 -0300

    commit

--- src/ui/User/CarouselSimilarProperties/CarouselSimilarProperties.tsx
+++ src/ui/User/CarouselSimilarProperties/CarouselSimilarProperties.tsx
@@ -22,61 +22,56 @@ type props = {
 
 const CarouselSimilarProperties = ({ data }: props) => {
   return (
-    <Carousel className="w-full ">
+    <Carousel className="w-full">
       <CarouselContent>
         {data?.map((item, index) => (
           <CarouselItem key={index}>
             <div className="p-1">
-              <Card
-                className={` hover:scale-101 h-auto transition-all duration-300 ease-in-out shadow-md cursor-pointer p-0 pb-2 w-[340px]`}
-              >
+              <Card className="hover:scale-101 w-full h-[400px] transition-all duration-300 ease-in-out shadow-md cursor-pointer p-0 pb-2 flex flex-col">
                 <CardHeader className="p-0 relative">
                   <div className="relative">
                     <Image
                       src={item.images[0]}
-                      width={100}
-                      height={200}
+                      width={340}
+                      height={250}
                       alt="image"
-                      className="w-full h-[250px] rounded-t-lg object-cover "
+                      className="w-full h-[250px] rounded-t-lg object-cover"
                     />
                   </div>
                   <div className="absolute bottom-3 left-1">
                     <Badge
-                      className={`${item.listingType === "RENT" ? "bg-blue-500" : "bg-green-500"}`}
+                      className={`${
+                        item.listingType === "RENT"
+                          ? "bg-blue-500"
+                          : "bg-green-500"
+                      }`}
                     >
                       {item.listingType === "RENT" && "Alquiler"}
                       {item.listingType === "SALE" && "Venta"}
                     </Badge>
                   </div>
                 </CardHeader>
-                <CardFooter className="grid p-2 m-0">
+
+                <CardFooter className=" grid p-2 m-0">
                   <div className="flex items-center gap-1">
                     <MapPin size={18} />
                     <span className="text-[10px] text-gray-600">
                       {item.neighborhood}, {item.city}
                     </span>
                   </div>
-                  <p className="text-[14px] font-bold ">{item.title}</p>
-                  <p className="text-[12px] mb-4 text-gray-600">
-                    {item.description.length > 120
-                      ? `${item.description.substring(0, 120)}...`
-                      : item.description}
+                  <p className="text-[14px] font-bold">{item.title}</p>
+                  <p className="text-[12px] mb-2 text-gray-600 line-clamp-3">
+                    {item.description}
                   </p>
-                  <div className="flex items-center justify-between ">
-                    <div className=" flex items-center gap-2 text-[10px]">
-                      <span>{item.bedrooms} bedrooms</span>
-                      <span>{item.bathrooms} bathrooms</span>
+                  <div className="flex items-center justify-between mt-auto">
+                    <div className="flex items-center gap-2 text-[10px]">
+                      <span>{item.bedrooms} dormitorios</span>
+                      <span>{item.bathrooms} baños</span>
                       <span>{item.squareMeters} m²</span>
                     </div>
-                    <p className="text-1xl  flex flex-col items-center gap-0 md:flex-row md:gap-1 md:text-lg font-bold text-green-600">
-                      <span className="text-[14px]">
-                        {" "}
-                        {item.currency === "USD" ? "$ USD " : " $ ARG "}
-                      </span>
-                      <span className="text-[14px]">
-                        {" "}
-                        {item.price.toLocaleString("en-US")}
-                      </span>
+                    <p className="text-[14px] font-bold text-green-600">
+                      {item.currency === "USD" ? "$ USD" : "$ ARG"}{" "}
+                      {item.price.toLocaleString("en-US")}
                     </p>
                   </div>
                 </CardFooter>
